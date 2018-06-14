73f36771c99813cd79bd3f88ff732a5f3527803a
Krisztian Sandor Nagy <nagy_krisztian_sandor@yahoo.com>
2018-06-14 19:06:16 +0200

    added model

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secret':'verysecret',
+    'database': 'mongodb://localhost/stockdb'
+};
--- model/user.js
+++ model/user.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var bcrypt = require('bcrypt-nodejs');
 
-var UserSchema = new Schema({
+var User = new Schema({
     username: {
         unique: true,
         required: true,
@@ -14,7 +14,7 @@ var UserSchema = new Schema({
     }
 });
 
-UserSchema.pre('save', function (next) {
+User.pre('save', function (next) {
     var user = this;
     if (this.isModified('password') || this.isNew) {
         bcrypt.genSalt(10, function (err, salt) {
@@ -34,7 +34,7 @@ UserSchema.pre('save', function (next) {
     }
 });
 
-UserSchema.methods.comparePassword = function (passw, cb) {
+User.methods.comparePassword = function (passw, cb) {
     bcrypt.compare(passw, this.password, function (err, isMatch) {
         if (err) {
             return cb(err);
@@ -43,4 +43,4 @@ UserSchema.methods.comparePassword = function (passw, cb) {
     });
 };
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', User);
